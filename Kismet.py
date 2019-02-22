b3da8d09573e01cc204b8b83798f0ac0fd62acf2
QuantuMope <43973701+QuantuMope@users.noreply.github.com>
2019-02-21 23:59:40 -0800

    Restructured Furas Frame Uploading. Added Furas state change function. Needs additional work.

--- Kismet.py
+++ Kismet.py
@@ -22,7 +22,7 @@ class TiledMap:
                 for x, y, gid in layer:
                     tile = ti(gid)
                     if tile:
-                        surface.blit(tile, (x*self.tm.tilewidth,y*self.tm.tileheight))
+                        surface.blit(tile, (x * self.tm.tilewidth, y * self.tm.tileheight))
 
     def make_map(self):
         self.surface = pg.Surface((self.width, self.height))
@@ -32,50 +32,58 @@ class TiledMap:
 class Fursa_sprite(pg.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        # self.idle()
         self.frame_index = 0
-        self.movex = 1
-        self.movey = 1
-        self.state = self.idle()
+        self.upload_frames()
+        self.current_images = self.idle_images
+        self.image = self.idle_images[0]
+        self.rect = self.image.get_rect()
+        # self.change_state = True
+
+    def upload_frames(self):
+        self.idle_images = []
+        self.walk_images = []
+        self.all_images = [self.idle_images, self.walk_images]
+        self.directories = ["C:/Users/Andrew/Desktop/Python_Projects/Kismet/Furas/Idle"   # Idle Animation
+                           ,"C:/Users/Andrew/Desktop/Python_Projects/Kismet/Furas/Walk"]  # Walking Animation
+
+        for i, directory in enumerate(self.directories):
+            os.chdir(directory)
+            for file in os.listdir(directory):
+                self.all_images[i].append(pg.transform.scale(load_image(file), (156, 156)))
+        self.frame_index_max = 12
 
     def idle(self):
-        self.directory = "C:/Users/Andrew/Desktop/Python_Projects/Kismet/Furas/Idle"
-        os.chdir(self.directory)
-        self.images = []
-        for file in os.listdir(self.directory):
-            self.images.append(pg.transform.scale(load_image(file), (156, 156)))
-        self.frame_index_max = len(self.images)
-        self.image = self.images[0]
-        self.rect = self.image.get_rect()
+        return True
 
     def walk(self):
-        self.directory = "C:/Users/Andrew/Desktop/Python_Projects/Kismet/Furas/Walk"
-        os.chdir(self.directory)
-        self.images = []
-        for file in os.listdir(self.directory):
-            self.images.append(pg.transform.scale(load_image(file), (156, 156)))
-        self.frame_index_max = len(self.images)
-        self.image = self.images[0]
-        self.rect = self.image.get_rect()
+        return False
 
     def change_state(self):
-        self.placeholder = 0
+        if pg.key.get_focused():
+            self.current_images = self.walk_images
+        else:
+            self.current_images = self.idle_images
 
     def handle_keys(self):
+        pg.event.pump()
         keys = pg.key.get_pressed()
+        print(keys)
         dist = 3
         if keys[pg.K_UP]:
             self.rect.y -= dist
-        elif keys[pg.K_RIGHT]:
+        if keys[pg.K_RIGHT]:
             self.rect.x += dist
-        elif keys[pg.K_DOWN]:
+        if keys[pg.K_DOWN]:
             self.rect.y += dist
-        elif keys[pg.K_LEFT]:
+        if keys[pg.K_LEFT]:
             self.rect.x -= dist
 
     def update(self):
         self.handle_keys()
-        self.image = self.images[self.frame_index]
+        self.change_state()
+        print(pg.key.get_focused())
+
+        self.image = self.current_images[self.frame_index]
         self.frame_index += 1
         if self.frame_index == self.frame_index_max:
             self.frame_index = 0
@@ -99,7 +107,6 @@ def main():
 
     Starting_Area = Level_Start()
     Fursa = Fursa_sprite()
-    Fursa.walk()
     Sprites_list = pg.sprite.Group()
     Sprites_list.add(Fursa)
 
